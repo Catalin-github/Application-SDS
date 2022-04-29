6d53637c218793384088d520f5982f2235854cbc
Moholea Samuel-Catalin <66556043+Catalin-github@users.noreply.github.com>
2022-04-29 14:03:56 +0200

    Delete frontend/measure directory

--- frontend/measure/MeasureActionType.js
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-export const MEASURE_SUCCESS = 'MEASURE_SUCCESS';
-export const MEASURE_LOADING = 'MEASURE_LOADING';
-export const MEASURE_FAILURE = 'MEASURE_FAILURE';
-export const MEASURE_ADD = 'MEASURE_ADD';
-export const MEASURE_DELETE = 'MEASURE_DELETE';
